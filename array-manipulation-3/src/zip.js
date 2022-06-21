f410428460d8489feb005fdbbb9fb35c86babcd5
Joe King <tiptop1112@gmail.com>
2022-06-21 22:26:40 +0000

    array-manipulation-3 in progress

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -3,11 +3,15 @@ function zip(first, second) {
   var newArray = [];
   if (first.length <= second.length) {
     for (var i = 0; i < first.length; i++) {
-      newArray.push(first[i], second[i]);
+      var subArray = [];
+      subArray.push(first[i], second[i]);
+      newArray.push(subArray);
     }
   } else if (first.length >= second.length) {
     for (var c = 0; c < second.length; c++) {
-      newArray.push(first[c], second[c]);
+      subArray = [];
+      subArray.push(first[c], second[c]);
+      newArray.push(subArray);
     }
   } return newArray;
 }
