ab61dfdbddcbed4cff39dceecf3376da27ef411a
Joe King <tiptop1112@gmail.com>
2022-06-24 01:45:17 +0000

    array-manipulation-3 in progress.

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,13 @@
 /* exported unique */
+function unique(array) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < i; j++) {
+      if (array[i] === array[j]) {
+        break;
+      }
+    } if (i === j) {
+      newArray.push(array[i]);
+    }
+  } return newArray;
+}
